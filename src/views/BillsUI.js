fc935fa317ecca5389694b0b61c8ad86f3d7bb8a
AndreaP2A <andrea.porche2a@gmail.com>
2025-03-07 16:55:36 +0000

    Debug Login (Admin) & Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,31 +1,38 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
-
-import Actions from './Actions.js'
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
+import Actions from "./Actions.js";
 
 const row = (bill) => {
-  return (`
-    <tr>
-      <td>${bill.type}</td>
-      <td>${bill.name}</td>
-      <td>${bill.date}</td>
-      <td>${bill.amount} €</td>
-      <td>${bill.status}</td>
+  // La date sera affichée formattée (lisible) si c'est possible, sinon
+  // elle sera affichée telle quelle, elle sera alors affichée en tant que date "brute".
+  const billDate = bill.formatedDate ?? bill.date;
+  // "data-testid" a été ajouté pour faciliter les tests
+  return `
+    <tr data-testid="bill">
+      <td data-testid="type">${bill.type}</td>
+      <td data-testid="name">${bill.name}</td>
+      <td data-testid="date">${billDate}</td>
+      <td data-testid="amount">${bill.amount} €</td>
+      <td data-testid="status">${bill.status}</td>
       <td>
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  return data && data.length
+    ? data
+        .sort((a, b) => (a.date < b.date ? 1 : -1))
+        .map((bill) => row(bill))
+        .join("")
+    : "";
+};
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +47,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +82,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
