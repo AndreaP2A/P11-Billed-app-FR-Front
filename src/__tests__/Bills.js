9e24ee39accaa4ea31b95f1b3c62274756036b89
AndreaP2A <andrea.porche2a@gmail.com>
2025-03-13 17:26:37 +0000

    Ajout tests "click on New Bill" & "click on eye icon

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,12 +3,19 @@
  */
 
 import { screen, waitFor } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 import BillsUI from "../views/BillsUI.js";
+import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
+
+////// Tâche 3 [Tests unitaires et d’intégration]
+// Appui sur le mock de l'API
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -29,7 +36,9 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId("icon-window"));
       const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
+      ////// Tâche 3 [Tests unitaires et d’intégration]
+      // to-do write expect expression
+      expect(windowIcon).toHaveClass("active-icon");
     });
     test("Then bills should be ordered from most recent to oldest", () => {
       document.body.innerHTML = BillsUI({ data: bills });
@@ -38,11 +47,67 @@ describe("Given I am connected as an employee", () => {
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-      ////// Kanban [Bug report] - Bills
+      ////// Tâche 2 [Bug report] - Bills
       // const antiChrono = (a, b) => (a < b ? 1 : -1);
       const antiChrono = (a, b) => new Date(a.date) - new Date(b.date);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+
+    ////// Tâche 3 [Tests unitaires et d’intégration]
+    // Rendu du formulaire d'ajout en cliquant sur "nouvelle note de frais"
+    describe("When I click on the New Bill button", () => {
+      test("It should open the New Bill page", async () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({ type: "Employee" })
+        );
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          store: null,
+          localStorage: window.localStorage,
+        });
+        document.body.innerHTML = BillsUI({ data: bills });
+
+        const btnNewBill = await screen.getByTestId("btn-new-bill");
+        const handleClickNewBill = jest.fn(
+          () => billsContainer.handleClickNewBill
+        );
+        btnNewBill.addEventListener("click", handleClickNewBill);
+
+        userEvent.click(btnNewBill);
+        expect(handleClickNewBill).toHaveBeenCalled();
+      });
+    });
+    ////// Tâche 3 [Tests unitaires et d’intégration]
+    // Rendu de la modale (justificatif) en cliquant sur l'icone oeil (voir)
+    describe("When I click on the eye icon of a bill", () => {
+      test("It should open the modal with the bill's justification (img)", async () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({ type: "Employee" })
+        );
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          store: null,
+          localStorage: window.localStorage,
+        });
+        document.body.innerHTML = BillsUI({ data: bills });
+      });
     });
   });
 });
